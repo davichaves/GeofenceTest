9ea8f7b2626579758ba72a3e3b2a59585d655720
Davi Chaves <drodrigues@upstart13.com>
2022-01-26 17:13:23 -0300

    initial setup

--- GeofenceTest/ViewController.swift
+++ GeofenceTest/ViewController.swift
@@ -5,15 +5,42 @@
 //  Created by Davi Chaves on 1/26/22.
 //
 
+import CoreLocation
 import UIKit
 
 class ViewController: UIViewController {
+    
+    lazy var locationManager = CLLocationManager()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        locationManager.delegate = self
+        locationManager.requestAlwaysAuthorization()
     }
-
 
 }
 
+extension ViewController: CLLocationManagerDelegate {
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        let status = manager.authorizationStatus
+        if status != .authorizedAlways {
+            let message = "the app needs to access the device location."
+            let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+            let action = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+            alert.addAction(action)
+            present(alert, animated: true, completion: nil)
+        }
+    }
+
+    func locationManager(_ manager: CLLocationManager, monitoringDidFailFor region: CLRegion?, withError error: Error) {
+        guard let region = region else {
+            print("Monitoring failed for unknown region")
+            return
+        }
+        print("Monitoring failed for region with identifier: \(region.identifier)")
+    }
+
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("Location Manager failed with the following error: \(error)")
+    }
+}
