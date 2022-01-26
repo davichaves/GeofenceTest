7cafc660ba964b0f892daf4e81fd872ee75d8162
Davi Chaves <drodrigues@upstart13.com>
2022-01-26 17:07:43 -0300

    Initial Commit

---
+++ GeofenceTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GeofenceTest
+//
+//  Created by Davi Chaves on 1/26/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
